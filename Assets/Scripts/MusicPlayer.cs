dfd1ca004f66c1d72e9be4f80632afa5889a30eb
David Ouyang <douyang@blizzard.com>
2016-02-08 19:37:11 -0800

    Move singleton MusicPlayer destroy to Awake() to avoid small sound bug.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,22 +5,23 @@ public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start () {
-
+	void Awake () {
+		// Avoid small sound bug by destroying earlier in the lifecycle
 		if (instance != null) {
-
 			// Destroy this new game object to prevent duplicate.
 			Destroy (gameObject);
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad (gameObject);
-
 		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
