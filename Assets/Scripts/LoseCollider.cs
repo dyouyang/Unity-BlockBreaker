1d241db22bbc00488f80ee9e53190012909ff688
David Ouyang <douyang@blizzard.com>
2016-02-11 08:55:41 -0800

    Add a lose collider to load the end game screen.
    Begin adding a paddle

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		print ("Triggered");
+		levelManager.LoadLevel ("Win");
+	}
+}
