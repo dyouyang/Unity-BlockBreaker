5fd0f7666ca4791b45a59b30d2a5c177e3727272
David Ouyang <douyang@blizzard.com>
2016-02-12 19:28:46 -0800

    Fire ball off the paddle when clicked.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+
+	private Vector3 paddleToBallVector;
+
+	bool gameStarted = false;
+
+	// Use this for initialization
+	void Start () {
+
+		// Save the relative position of ball to paddle
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!gameStarted) {
+
+			// Keep ball sticky
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			// Launch ball.
+			if (Input.GetMouseButtonDown(0)) {
+				this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+				gameStarted = true;
+			}
+		}
+	}
+}
